448285ebe4d9922eac77c16e84f8573b697ef3c6
Anthony <sean.a.protho1@gmail.com>
2020-04-28 11:50:50 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+// src/Title.js
+
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>First_Flask</h1>
+    </div>,
+    <div className="Title-Subtitle">
+        <h3>"San Francisco Privately
+        Owned Public Open Spaces".</h3>
+    </div>
+  )
+}
+
+export default Title
